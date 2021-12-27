fd37084f561e3db09087552f9b1876565813f26e
eulersexception <bahadir.suezer@outlook.com>
2021-12-27 14:02:41 +0100

    💄 converted js to ts, removed code,setup chakraui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
